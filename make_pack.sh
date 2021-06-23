185c1da1d8f8401f0693c518d09d2e321f5d560d
Masterchef365 <duncan.freeman1@gmail.com>
2021-06-23 00:36:37 -0700

    Make transparent the default

--- make_pack.sh
+++ make_pack.sh
@@ -1,12 +1,32 @@
 #!/usr/bin/env bash
-quality="20%"
-tmp="/tmp/out.jpg"
+
+# Settings
+quality="1%"
+upscale="200%"
+tmp_color="/tmp/tmp_color.jpg"
+tmp_alpha="/tmp/tmp_alpha.jpg"
+
+# Copy pack
 rm -rf scuffed_pack/
 cp -r ./default_pack/ scuffed_pack/
+
+# Exclude spectific paths
+rm -r scuffed_pack/assets/minecraft/textures/colormap/
+
+# Loop over PNG images
 for image in $(find scuffed_pack/ -type f -name "*.png"); do
     echo $image
-    convert $image -resize 200% -quality $quality $tmp
-    convert $tmp $image
+    # Extract color channels and jpeg
+    convert $image -resize $upscale -quality $quality $tmp_color
+
+    # Extract alpha channel (with nearest resize)
+    convert $image -resize $upscale -interpolate Nearest -filter point -alpha extract $tmp_alpha
+
+    # Merge alpha and color channels
+    convert $tmp_color $tmp_alpha -alpha off -compose CopyOpacity -composite $image
+    #rm $tmp_color $tmp_alpha 
 done
-zip -r scuffed.zip scuffed_pack/
-rm -rf scuffed_pack/
+
+# Save to zip
+#zip -r scuffed.zip scuffed_pack/*
+#rm -rf scuffed_pack/
